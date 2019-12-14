8e18b323ef75f3ea003ef86d746975622c3b6f76
Kristian Antic <52401748+krantic@users.noreply.github.com>
2019-12-14 12:14:48 +0100

    Update IPv6Tunnel.ps1
    -Added Strict mode --> Ensure that programming best practices are followed
    -Fixed bugs with uninitialized variables
    -Extended PowerShell runtime environment detection

--- IPv6Tunnel.ps1
+++ IPv6Tunnel.ps1
@@ -6,41 +6,81 @@
 # based on https://tunnelbroker.net ->Tunnel Details
 #  -> Example Configurations Tab -> Windows 10 selection
 
+# Set strict mode to latest version (>=2.0)
+# Ensure that programming best practices are followed (e.g. prohibit uninitialized variables)
+Set-StrictMode -Version Latest
+
 # Pre-run check: Checks the minimal version of Windows PowerShell
 #Requires -Version 4.0
 Write-Output "This Script requires PowerShell Version >= 4.0"
-Write-Output "You are running PowerShell Version $($PSVersionTable.PSVersion)."
+Write-Output "You are running PowerShell $($PSVersionTable.PSEdition) Version $($PSVersionTable.PSVersion)"
+
+# Variables Initialization
+[bool]$PowerShell = $false
+[bool]$PowerShellCore = $false
+[bool]$PowerShell_ISE = $false
+[bool]$Administrator = $false
 
 # Checks the runtime environment of the script
-if ($host.name -eq 'ConsoleHost') {
-    [bool]$PowerShell = $true
-} 
-if ($host.name -eq 'Windows Powershell ISE Host') {
-    [bool]$PowerShell_ISE = $true
+if ((($host.name -eq 'ConsoleHost') -XOR ($host.name -like 'Visual Studio*')) -AND ($PSVersionTable.PSEdition -ne "Core")){
+    $PowerShell = $true
+    Write-Verbose "Running PowerShell=$($PowerShell)"
 }
+elseif ((($host.name -eq 'ConsoleHost') -XOR ($host.name -like 'Visual Studio*')) -AND ($PSVersionTable.PSEdition -eq "Core")){
+    $PowerShellCore = $true
+    Write-Verbose "Running PowerShell Core=$($PowerShellCore)"
+        if ($IsWindows) {
+            Write-Output  "System is running on PowerShell $($PSVersionTable.PSEdition) on platform $($PSVersionTable.Platform)=Win32NT(Windows). Script started."
+            }
+        else{
+            throw "System is not running PowerShell on $($PSVersionTable.PSEdition) on platform $($PSVersionTable.Platform)!=Win32NT(Windows). Script execution aborted."
+        }
+}
+elseif ($host.name -eq 'Windows Powershell ISE Host') {
+    $PowerShell_ISE = $true
+    Write-Verbose "Running PowerShell ISE=$($PowerShell_ISE)"
+}
+else {  
+        Write-Warning  "Unknown PowerShell runtime environment"
+} 
 
 # Check if we are currently running the runtime environment with elevated privileges as administrator.
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
-    [bool]$Administrator = $false
+if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    $Administrator = $true
 }
+else {
+    $Administrator = $false
+} 
 
-# Check if we are currently running PowerShell with elevated privileges as administrator.
+# Check if we are currently running Windows PowerShell with elevated privileges as administrator.
 # If not it is self-elevating while preserving the working directory.
-if ($PowerShell -and ($Administrator -eq $false)) {
+if (($PowerShell -eq $true) -AND ($Administrator -eq $false)){
+    Write-Warning  "Running Windows PowerShell without admin rights! Restarting PowerShell with admin rights!!!"
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
-    exit;
+    Exit
 }
+# Check if we are currently running PowerShell Core with elevated privileges as administrator.
+# If not it is self-elevating while preserving the working directory.
+if (($PowerShellCore -eq $true) -AND ($Administrator -eq $false)){
+    Write-Warning  "Running PowerShell Core without admin rights! Restarting PowerShell Core with admin rights!!!"
+    Start-Process pwsh -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    Exit
+}
+# Write-Host "PS Core: If you see this, the Exit didn't work..."
 
 # Check if we are currently running PowerShell ISE with elevated privileges as administrator.
 # If not it is self-elevating and starting PowerShell_ISE with the current Script.
 # Though you have to re-run the script manually.
-if ($PowerShell_ISE -and ($Administrator -eq $false)) {
+if (($PowerShell_ISE -eq $true) -AND ($Administrator -eq $false)) {
+    Write-Warning "Running script in PowerShell ISE without admin rights! Restarting PowerShell ISE with admin rights!!!"
+    Write-Warning "Please re-run script manually."
     Start-Process PowerShell_ISE -Verb RunAs "-NoProfile -File $PSCommandPath" 
-    exit;
+    Exit
 }
+#Write-Host "ISE: If you see this, the Exit didn't work..."
 
 # Windows PowerShell ISE doesn't like netsh/netsh.exe, we'll fix that:
-if ($PowerShell_ISE) {
+if ($PowerShell_ISE -eq $true) {
     $psUnsupportedConsoleApplications.Remove("netsh")
     $psUnsupportedConsoleApplications.Remove("netsh.exe")
 } 
@@ -57,11 +97,11 @@ $USERNAME = ""
 # Tunnel specific authentication key (Update Key. See Tunnel Details 
 # -> Advanced tab on the tunnel information page) 
 # if one is set, otherwise your tunnelbroker.net password.
-$UPDATEKEY_OR_PASSWORD =  ""
+$UPDATEKEY_OR_PASSWORD = ""
 #
 # Your Numeric Tunnel ID (unique identifier for your tunnel. See Tunnel Details 
 # -> IPv6 Tunnel tab on the tunnel information page)
-$TUNNEL_ID =  ""
+$TUNNEL_ID = ""
 #
 # Update URL
 # Used to update the listed tunnel's client endpoint to the IP address making the update request.
@@ -71,7 +111,7 @@ $URL = "https://ipv4.tunnelbroker.net/nic/update?username=$USERNAME&password=$UP
 # See Tunnel Details 
 # -> IPv6 Tunnel tab ->IPv6 Tunnel Endpoints section on the tunnel information page)
 # Server IPv4 Address 
-# This is the IPv4 endpoint of your Tunnel Server. 
+# This is the IPv4 endpoint of your Tunnel Server.
 $ServerIPv4Address = "X.X.X.X"
 # Server IPv6 Address
 # This is the IPv6 endpoint of your Tunnel on our Tunnel Server.(/64 allocation)
@@ -134,12 +174,12 @@ netsh interface ipv6 add route prefix=::/0 interface=$TUNNELNAME nexthop=$Server
 
 # Opened admin PowerShell waits for a key press and doesn't close automatically.
 # used if the script is running in PowerShell
-if ($PowerShell) {
+if (($PowerShell -eq $true) -OR ($PowerShellCore -eq $true)) {
     Write-Output "Press any key to close this shell..."
     $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 }
 # used if the script is running in PowerShell ISE
-if ($PowerShell_ISE) {
+if ($PowerShell_ISE -eq $true) {
     Read-Host "Press ENTER key to close this PowerShell ISE shell..."
     # Cleanup-add netsh and netsh.exe again to $psUnsupportedConsoleApplications
     $psUnsupportedConsoleApplications.Add("netsh")
